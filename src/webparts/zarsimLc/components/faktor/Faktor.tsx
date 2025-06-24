329ba46536de930769b9f8830af0f049c1312c0b
arashms1994 <arash.ms1994@gmail.com>
2025-06-24 16:48:55 +0330

    Feat: Add faktor and faktor details but it has some problems

--- src/webparts/zarsimLc/components/faktor/Faktor.tsx
+++ src/webparts/zarsimLc/components/faktor/Faktor.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styles from "./Faktor.module.scss";
 
 export default class Faktor extends React.Component<any, any> {
   constructor(props) {
@@ -7,8 +8,40 @@ export default class Faktor extends React.Component<any, any> {
 
   public render() {
     const { customer } = this.props;
-    console.log(customer);
+    
+    if (!customer || Object.keys(customer).length === 0) {
+        return <div>در حال بارگذاری...</div>;
+    }
+    
+    console.log(customer.item);
+    return (
+      <div className={styles.faktorContainer}>
+        <div className={styles.faktorHeaderDiv}>
+          <h1>مشخصات مشتری</h1>
+        </div>
 
-    return <div>oooooooooooooollllllllll</div>;
+        <div className={styles.faktorHero}>
+          <div className={styles.faktorParaphDiv}>
+            <p className={styles.faktorLabel}>شماره پیش فاکتور:</p>
+            <p className={styles.faktorParaph}>{customer.item.Title}</p>
+          </div>
+
+          <div className={styles.faktorParaphDiv}>
+            <p className={styles.faktorLabel}>تاریخ ثبت پیش فاکتور:</p>
+            <p className={styles.faktorParaph}>{customer.item.Date}</p>
+          </div>
+
+          <div className={styles.faktorParaphDiv}>
+            <p className={styles.faktorLabel}>نام مشتری:</p>
+            <p className={styles.faktorParaph}>{customer.item.Customer}</p>
+          </div>
+
+          <div className={styles.faktorParaphDiv}>
+            <p className={styles.faktorLabel}>نوع پیش فاکتور:</p>
+            <p className={styles.faktorParaph}>{customer.item.type_factor}</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/webparts/zarsimLc/components/faktor/FaktorDetail.tsx
+++ src/webparts/zarsimLc/components/faktor/FaktorDetail.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styles from "./Faktor.module.scss";
 
 export default class FaktorDetail extends React.Component<any, any> {
   constructor(props) {
@@ -7,7 +8,28 @@ export default class FaktorDetail extends React.Component<any, any> {
 
   public render() {
     const { products } = this.props;
+
+    if (!products || Object.keys(products).length === 0) {
+      return <div>در حال بارگذاری...</div>;
+    }
+
     console.log(products);
-    return <div>pppppppprrrrrrrrrrr</div>;
+    return (
+      <div className={styles.faktorContainer}>
+        <div className={styles.faktorHeaderDiv}>
+          <h1>جزئیات پیش فاکتور</h1>
+        </div>
+        {products.map((p) => (
+          <div style={{display:"flex", justifyContent:"space-around"}} key={p.id}>
+            <p>{p.Title}</p>
+            <p>{p.colertitle}</p>
+            <p>{p.Category}</p>
+            <p>{p.Packing}</p>
+            <p>{p.Price}</p>
+            <p>{p.Value}</p>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
