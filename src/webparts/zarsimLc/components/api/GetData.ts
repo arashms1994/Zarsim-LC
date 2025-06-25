54c29ef2ec41e85eed48cfb1ccdc7a2723b50218
arashms1994 <arash.ms1994@gmail.com>
2025-06-25 11:19:05 +0330

    fix: get issue for sharepoint limitation

--- src/webparts/zarsimLc/components/api/GetData.ts
+++ src/webparts/zarsimLc/components/api/GetData.ts
@@ -73,21 +73,26 @@ export async function getCustomerFactor(factorNumber: string) {
 
 export async function getCustomerFactorDetails(factorNumber: string) {
   const listTitle = "detail_customer_factor";
+  let allResults = [];
+  let nextUrl = `${BASE_URL}/_api/web/lists/getbytitle('${listTitle}')/items?$filter=OrderNumber eq '${factorNumber}'`;
 
   try {
-    const response = await fetch(
-      `${BASE_URL}/_api/web/lists/getbytitle('${listTitle}')/items?$filter=OrderNumber eq '${factorNumber}'`,
-      {
+    while (nextUrl) {
+      const response = await fetch(nextUrl, {
         method: "GET",
         headers: {
           Accept: "application/json;odata=verbose",
         },
-      }
-    );
+      });
 
-    const data = await response.json();
+      const data = await response.json();
 
-    return data.d.results;
+      allResults = [...allResults, ...data.d.results];
+
+      nextUrl = data.d.__next || null;
+    }
+
+    return allResults;
   } catch (err) {
     console.error("خطا در دریافت آیتم‌ها:", err);
     return [];
